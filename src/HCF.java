c8af4dbda8a76facfd1a9616ad349ff17b39d620
josemartinthomas14 <josemartinthomas14@example.com>
2026-04-06 20:59:01 +0530

    Solve experiment 4

--- src/HCF.java
+++ src/HCF.java
@@ -1,5 +1,18 @@
+import java.util.Scanner;
+
 public class HCF {
-    
-        // TODO: Read two integers and print their HCF
-    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        if (scanner.hasNextInt()) {
+            int a = scanner.nextInt();
+            int b = scanner.nextInt();
+            while (b != 0) {
+                int temp = b;
+                b = a % b;
+                a = temp;
+            }
+            System.out.println(a);
+        }
+        scanner.close();
+    }
 }
